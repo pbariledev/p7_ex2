c45073893895478d31af694f8d1e514422ea425f
Pierre BARILE <pbariledev@gmail.com>
2022-07-29 00:02:50 +0200

    finish

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,36 +4,48 @@ import styled from 'styled-components'
 import colors from '../../utils/colors'
 
 const CardLabel = styled.span`
-    color: #5843e4;
-    font-size: 22px;
-    font-weight: bold;
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: normal;
+  padding-left: 15px;
 `
+
+const CardTitle = styled.span`
+  color: black;
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+`
+
 const CardImage = styled.img`
-height: 80px;
-width: 80px;
-border-radius: 50%;
+  height: 150px;
+  width: 150px;
+  align-self: center;
+  border-radius: 50%;
 `
 
 const CardWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 15px;
-    background-color: ${colors.backgroundLight};
-    border-radius: 30px;
-    width: 350px;
-    transition: 200ms;
-    &:hover {
-        cursor: pointer;
-        box-shadow: 2px 2px 10px #e2e3e9;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 15px;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  width: 300px;
+  height: 300px;
+  transition: 200ms;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 2px 2px 10px #e2e3e9;
+  }
 `
 
 function Card({ label, title= 'titre par defaut', picture }) {
     return (
-        <CardWrapper style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+        <CardWrapper >
             <CardLabel>{label}</CardLabel>
-            <CardImage src={picture} alt="freelance" height={80} width={80} />
-            <span>{title}</span>
+            <CardImage src={picture} alt="freelance" />
+            <CardTitle>{title}</CardTitle>
         </CardWrapper>
     )
 }
