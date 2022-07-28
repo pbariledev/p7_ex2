683407dc49ca78b568978576f739153bb03d70b3
Pierre BARILE <pbariledev@gmail.com>
2022-07-28 23:34:07 +0200

    style global

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,6 +1,8 @@
 import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card'
- 
+
+import styled from 'styled-components'
+
 const freelanceProfiles = [
     {
         name: 'Jane Doe',
@@ -19,21 +21,28 @@ const freelanceProfiles = [
     },
 ]
 
+const CardsContainer = styled.div`
+    display: grid;
+    gap: 24px;
+    grid-template-rows: 350px 350px;
+    grid-template-columns: repeat(2, 1fr);
+`
 
 function Freelances() {
-  return (
-      <div>
-          <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-          {freelanceProfiles.map((profile, index) => (
-              <Card
-                  key={`${profile.name}-${index}`}
-                  label={profile.jobTitle}
-                  picture={profile.picture}
-                  title={profile.name}
-              />
-          ))}
-      </div>
-  )
-}  
+    return (
+        <div>
+            <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+            <CardsContainer>
+                {freelanceProfiles.map((profile, index) => (
+                    <Card
+                        key={`${profile.name}-${index}`}
+                        label={profile.jobTitle}
+                        title={profile.name}
+                    />
+                ))}
+            </CardsContainer>
+        </div>
+    )
+}
 
   export default Freelances
